b16cbfec8beaedc5b7c9fac2ce23826c05c598d3
xti9er <xti9er@gmail.com>
2015-06-19 16:12:44 +0800

    Update process_list.c

--- process_list.c
+++ process_list.c
@@ -1,4 +1,4 @@
-//by xti9er @ 2015.4.27
+//by xti9er @ 2015.6.19
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
@@ -7,6 +7,10 @@
 #include <linux/sched.h>
 #include <linux/stat.h>
 #include <linux/version.h>
+#include <asm/current.h>
+#include <linux/pid.h>
+
+//#include <linux/pid_namespace.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 24)
 #define ITERATE_NAME readdir
@@ -27,22 +31,59 @@ void *get_vfs_readdir ( const char *path )
 
     if ( (filep = filp_open(path, O_RDONLY, 0)) == NULL )
         return NULL;
-        if(!IS_ERR(filep)){
-            if ((ret = filep->f_op->ITERATE_NAME)==NULL)
-                return NULL;
-            filp_close(filep, 0);
-            return ret;
+    if(!IS_ERR(filep)){
+        if ((ret = filep->f_op->ITERATE_NAME)==NULL)
+            return NULL;
+        filp_close(filep, 0);
+        return ret;
+    }
+    else{
+        return NULL;
+    }
+}
+
+struct task_struct *x_find_task_by_pid(pid_t nr)
+{
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 27)
+	return find_task_by_pid(nr);
+#else
+    struct pid *pid;
+    struct task_struct *ts = NULL;
+	pid = find_get_pid(nr);
+	if(pid) {
+		ts = pid_task(pid,PIDTYPE_PID);
+		put_pid(pid);
+	}
+	return ts;
+#endif
+}
+
+int find_task_list(pid_t nr){
+	struct task_struct *task_list;
+	char path[64]="\0";
+        for_each_process(task_list) {
+		if(task_list->pid==nr){
+        	        strcpy(path,"/proc/");
+                	sprintf(path,"%s%d",path,nr);
+
+                        if(get_vfs_readdir(path)){
+				return nr;
+                        }
+                        else{
+				return -1;
+                        }
+		}
         }
-        else{
-                return 0;
-        }
+
+	return 0;
 }
 
 int read_proc(READ_PROC_PROTO) 
 {
 	int len=0;
-	struct task_struct *task_list;
-	char path[64]="\0";
+	pid_t p=1;
+	
+	struct task_struct *t;
 
 	if(read_flag)
 		read_flag = 0;
@@ -50,23 +91,45 @@ int read_proc(READ_PROC_PROTO)
 		read_flag = 1;
 		return 0;
 	}
-	len  += sprintf(buffer+len, "\t\t\tRootkit Checker Demo\n\t\t\tbeta v0.1 [only vfs hook rootkit]\tby xti9er\n");
-	for_each_process(task_list) {
-		strcpy(path,"/proc/");
-		sprintf(path,"%s%d",path,task_list->pid);
-		len  += sprintf(buffer+len, "[%d] %s",task_list->pid,task_list->comm);
-        printk("[%d] %s",task_list->pid,task_list->comm);
+	len  += sprintf(buffer+len, "\t\t\t--= Xt9 - Anti - Rootkit =--\n\t\t\tbeta v0.11\tby xti9er\n");
 
-			if(get_vfs_readdir(path)){
-				len  += sprintf(buffer+len, "\n");
-				printk("\n");
+
+	while(p< PID_MAX_LIMIT ){
+
+		if(t=x_find_task_by_pid(p)){	
+			
+			if(t->tgid!=p)	//thread id
+				goto N;
+		
+			int p_flag=find_task_list(p);
+
+			if(!x_find_task_by_pid(p)){	//when the process is stopped
+//				printk("skip %d\n",p);
+				goto N;
+			}
+
+			if(p_flag==-1){
+				len  += sprintf(buffer+len, "[%d] %s\t[may be hiddened by vfs_readdir hook]\n",t->pid,t->comm);
+				printk("[%d] %s\t[maybe hiddened by vfs_readdir hook]\n",t->pid,t->comm);
+			}
+			else if(p_flag==0){
+				len  += sprintf(buffer+len, "[%d] %s\t[may be hiddened by REMOVE_LINKS]\n",t->pid,t->comm);
+				printk("[%d] %s\t[maybe hiddened by REMOVE_LINKS]\n",t->pid,t->comm);
 			}
 			else{
-				len  += sprintf(buffer+len, "\t[may be hiddened by vfs_readdir hook]\n");
-				printk("\t[maybe hiddened by vfs_readdir hook]\n");
+		        	//printk("\n");
+//	                	len  += sprintf(buffer+len, "[%d] (%d)\n",t->pid,p_flag);
 			}
+		}
+	
+/*	
+		else{
+			printk("[%d] failed\n",i);
+		}
+*/
+		N:
+		p++;
 	}
-	  
 	return len;
 }
 
